7a1db46b7afe3dbf5d39aa65db7867d9743ec035
Topofthemountain <jefferymiles333@gmail.com>
2025-01-01 20:20:40 +0300

    some more changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Remove the server configuration for production builds
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
 });
